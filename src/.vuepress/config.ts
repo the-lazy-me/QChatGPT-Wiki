da545ce1ecba6f5ffb0df836876cd6f6b14808d5
lazy <52873503+the-lazy-me@users.noreply.github.com>
2023-11-11 20:59:24 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-import { searchProPlugin } from "vuepress-plugin-search-pro";
+//import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
   base: "/",
@@ -10,23 +10,23 @@ export default defineUserConfig({
   description: "QChatGPT项目的Wiki页",
 
   theme,
-  plugins: [
-    searchProPlugin({
+  //plugins: [
+  //  searchProPlugin({
       // 索引全部内容
-      indexContent: true,
+  //    indexContent: true,
       // 为分类和标签添加索引
-      customFields: [
+  //    customFields: [
         {
-          getter: (page) => page.frontmatter.category,
-          formatter: "分类：$content",
-        },
-        {
-          getter: (page) => page.frontmatter.tag,
-          formatter: "标签：$content",
-        },
-      ],
-    }),
-  ],
+  //        getter: (page) => page.frontmatter.category,
+  //        formatter: "分类：$content",
+  //      },
+  //      {
+  //        getter: (page) => page.frontmatter.tag,
+  //        formatter: "标签：$content",
+  //      },
+  //    ],
+  //  }),
+  //],
   // Enable it with pwa
   // shouldPrefetch: false,
   head: [
